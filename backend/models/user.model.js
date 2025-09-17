fcba17fd5718ae7eb50ddb3e6b32bfd86b1e4258
Bitan-Banerjee244 <bbitan244@gmail.com>
2025-09-17 12:14:01 +0530

    feat(model) : Creating user model in user.model.js

---
+++ backend/models/user.model.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+
+let userSchema = new mongoose.Schema({
+    userName: {
+        type: String,
+        required: [true, "Please Enter Your Name"]
+    },
+    email: {
+        type: String,
+        required: [true, "Please Enter Your Email"],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, "Enter Your Password"],
+        minlength: [6, "Password must be at least 6 characters"],
+        maxlength: [64, "Password cannot exceed 64 characters"]
+    },
+    listings: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Listing"
+    }],
+    bookings: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Listing"
+    }]
+
+}, { timestamps: true })
+
+let User = mongoose.model("User", userSchema);
+export default User;
