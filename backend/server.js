e079cb678715d3ee05c3c0735545a6baf61670c1
Bitan-Banerjee244 <bbitan244@gmail.com>
2025-09-17 11:53:05 +0530

    feat(server.js) : Set up the server

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,15 @@
+import express from "express"
+import "dotenv/config"
+import cors from "cors"
 
+const app = express();
+let port = process.env.PORT || 3000
+
+
+app.get("/", (req, res) => {
+    res.send("Server Started");
+})
+
+app.listen(port, () => {
+    console.log(`Server started ta http://localhost:${port}`);
+})
