5ddd6209531412af87f38f19eb1977a6f0bef856
Bitan-Banerjee244 <bbitan244@gmail.com>
2025-09-17 12:04:48 +0530

    feat(db && server.js): Set Up Mongodb connection

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,21 @@
 import express from "express"
 import "dotenv/config"
 import cors from "cors"
+import connectDB from "./db/db";
+import cookieParser from "cookie-parser";
 
 const app = express();
-let port = process.env.PORT || 3000
+let port = process.env.PORT || 3000;
 
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
     res.send("Server Started");
 })
 
 app.listen(port, () => {
+    connectDB();
     console.log(`Server started ta http://localhost:${port}`);
 })
