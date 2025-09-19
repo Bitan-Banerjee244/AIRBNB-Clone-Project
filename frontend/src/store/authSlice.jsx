ac080935581cf93f3f78f3da454a5790e44d3406
Bitan-Banerjee244 <bbitan244@gmail.com>
2025-09-19 19:16:20 +0530

    feat(frontend):Setting Up Redux and redux toolkit for frontend

---
+++ frontend/src/store/authSlice.jsx
@@ -0,0 +1,10 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+let userSLice = createSlice({
+  name: "user",
+  initialState: {},
+  reducers: {},
+});
+
+export const {} = userSLice.actions;
+export default userSLice;
